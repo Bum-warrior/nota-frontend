305d2c0100e0905847dc886db652578b870ec788
Bum-warrior <andruazq34@gmail.com>
2022-03-13 20:08:28 +0300

    replace custom md for lib

--- src/components/TextEditor/TextEditor.tsx
+++ src/components/TextEditor/TextEditor.tsx
@@ -5,6 +5,9 @@ import TextDisplay from './TextDisplay';
 import axios from 'axios';
 import config from '../../config';
 import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+
+
 
 interface TextEditorProps {
     file?: IFile;
@@ -15,26 +18,13 @@ interface TextEditorProps {
 const TextEditor: React.FunctionComponent<TextEditorProps> = (props: TextEditorProps) => {
     const [text, settext] = useState('');
 
-    function saveChange(e: ContentEditableEvent){
-        let newValue : string = e.currentTarget.innerHTML;
-        // wrap first string in div
-        if(newValue[0] !== '<'){
-            console.log("REPLACER WORKED")
-            let mathString = '';
-            for(let i = 0; i < newValue.length; i++){
-                if(newValue[i] !== '<'){
-                    mathString = mathString + newValue[i];
-                } else {
-                    break;
-                }
-            }
-            newValue = newValue.replace(mathString, `<div>${mathString}</div>`)
-        }
+    function saveChange(text: string){
         
-        settext(newValue);
         if(props!=undefined && props.file!=undefined){
-            props.file.text=newValue;
+            props.file.text=text;
         }
+        settext(text);
+        
     }
 
     useEffect(() => {
@@ -43,18 +33,30 @@ const TextEditor: React.FunctionComponent<TextEditorProps> = (props: TextEditorP
 
     return (
         <div className='text-editor-container'>
-            <div className='text-editor-display-wrapper'>
-                <ContentEditable
+            <div className='text-editor-half-screen'>
+                {/* <ContentEditable
                     className='text-editor'
                     html={text} // innerHTML of the editable div
                     disabled={props.file? false : true}       // use true to disable editing  /  if file exist disable == false
                     onChange={(e) => {saveChange(e)}} // handle innerHTML change
                     tagName='article' // Use a custom HTML tag (uses a div by default)
-                />
+                /> */}
+                <div className='text-editor-field-wrapper'>
+                    <textarea 
+                    className='text-editor-field'
+                    value={text}
+                    onChange={(e) => {
+                        saveChange(e.target.value)
+                    }}
+                    />
+                </div>
             </div>
-            <div className='text-editor-display-wrapper'>
-                <TextDisplay displayText={text}/>
-                
+            <div className='text-editor-half-screen'>
+                <div className='text-editor-display-wrapper'>
+                    <div className='text-editor-display'>
+                        <ReactMarkdown remarkPlugins={[remarkGfm]}>{text}</ReactMarkdown>
+                    </div>
+                </div>
             </div>
         </div>
     );
